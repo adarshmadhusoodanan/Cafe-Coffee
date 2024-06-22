584d94d4bf3113bca8a107b50d62c93fb873af8b
Adarsh Madhusoodanan <iadarshmadhusoodanan@gmail.com>
2024-06-22 14:48:27 +0530

    add menu

--- script.js
+++ script.js
@@ -41,6 +41,7 @@ gsap.from('.title',{opacity: 0, duration: 1, delay: 1.6, y:30})
 gsap.from('.description',{opacity: 0, duration: 1, delay: 1.8, y:30})
 gsap.from('.btn',{opacity: 0, duration: 1, delay: 2.1, y:30})
 gsap.from('.image',{opacity: 0, duration: 1, delay: 2.3, y:30})
+gsap.from('.body .menu',{opacity: 0, duration: 1, delay: 1.8, y:30})
 
 
 
